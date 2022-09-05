e0b763291cb859547959b806213305f84e3b8f3e
Pignataro67 <Pignataro67@gmail.com>
2022-09-05 13:14:58 -0700

    JS

--- sript.js
+++ sript.js
@@ -5,8 +5,17 @@ const sizeEL = document.getElementById('size');
 const colorEl = document.getElementById('color');
 const clearEl = document.getElementById('clear');
 
+const ctx = canvas.getContext('2d');
+
 let isPressed = false
 colorEl.value = 'black'
 let color = colorEl.value
 let x
 let y
+
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true
+
+  x = e.offsetX
+  y = e.offsetY
+})
