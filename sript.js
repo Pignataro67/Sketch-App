95c70811a87d7cdeb2e8eb5f404438e5df54011a
Pignataro67 <Pignataro67@gmail.com>
2022-09-06 12:21:21 -0700

    JS

--- sript.js
+++ sript.js
@@ -19,3 +19,10 @@ canvas.addEventListener('mousedown', (e) => {
   x = e.offsetX
   y = e.offsetY
 })
+
+document.addEventListener('mouseup', (e) => {
+    isPressed = false
+
+    x = undefined
+    y = undefined
+})
